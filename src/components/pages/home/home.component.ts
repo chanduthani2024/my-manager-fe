9da408c6af8b84d2342f2d6be698e54d0805b26a
chandu thani <chandu@divami.com>
2024-09-10 14:57:33 +0530

    initial change

---
+++ src/components/pages/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  totalAmount:number = 0;
+  amountToDeduct:number = 0;
+  afterDeductions:number = 0;
+  amountSpent:number = 0;
+  progress:String = '';
+  status:String = '';
+  spendingReason:String = ''; 
+
+  constructor(private modalService: NgbModal) { }
+
+  calculateAfterDeductions(){
+    this.afterDeductions = this.totalAmount - this.amountToDeduct;
+  }
+  openModel(spendingItem:any){
+    // this.modalService.open(spendingItem, {
+    //   size: 'md',
+    //   ariaLabelledBy: 'modal-basic-title',
+    //   backdrop: 'static',
+    //   keyboard: false,
+    //   windowClass: 'Heat_submission-class'
+    // })
+  }
+}

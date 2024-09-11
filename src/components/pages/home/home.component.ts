51959154875815cd49d0b40b6f7b3ec7ac2e9e3f
chandu thani <chandu@divami.com>
2024-09-11 14:11:37 +0530

    third commit

--- src/components/pages/home/home.component.ts
+++ src/components/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -14,20 +15,24 @@ export class HomeComponent {
   amountSpent:number = 0;
   progress:String = '';
   status:String = '';
-  spendingReason:String = ''; 
+  spendingReason:String = '';
+  spendingDetails:any = [];
 
   constructor(private modalService: NgbModal) { }
 
   calculateAfterDeductions(){
     this.afterDeductions = this.totalAmount - this.amountToDeduct;
+    this.spendingDetails = [...this.spendingDetails, {amountToDeduct: this.amountToDeduct, 
+      spendingReason: this.spendingReason, status: this.status,amountSpent:this.amountSpent,progress:this.progress}];
+    this.modalService.dismissAll();
   }
   openModel(spendingItem:any){
-    // this.modalService.open(spendingItem, {
-    //   size: 'md',
-    //   ariaLabelledBy: 'modal-basic-title',
-    //   backdrop: 'static',
-    //   keyboard: false,
-    //   windowClass: 'Heat_submission-class'
-    // })
+    this.modalService.open(spendingItem, {
+      size: 'md',
+      ariaLabelledBy: 'modal-basic-title',
+      backdrop: 'static',
+      keyboard: false,
+      windowClass: 'Heat_submission-class'
+    })
   }
 }
